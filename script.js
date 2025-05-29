e892e780a1025d96a628c4bc2c8b63b4a3e8ebdf
Dhanu-prakash <dhanuprakash.edu@gmail.com>
2025-05-29 13:11:01 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ function render() {
 
   for (let i = 0; i < expenseList.length; i++) {
     let li = document.createElement("li");
-    li.textContent = `Rs${expenseList[i].amount} ${expenseList[i].desc}`;
+    
+
+    li.textContent = `Rs${expenseList[i].amount}        x${expenseList[i].quantity}       ${expenseList[i].desc}`;
 
     // Delete Button
     let delBtn = document.createElement("button");
@@ -20,9 +22,15 @@ function render() {
       localStorage.setItem("expense", JSON.stringify(expenseList));
       render();
     });
-
     li.appendChild(delBtn);
-    ul.appendChild(li); // ✅ Append li to ul
+    ul.appendChild(li); // 
+  //total Amount
+  let totalAmount = 0;
+  totalAmount += expenseList[i].amount * expenseList[i].quantity;
+ 
+let total = document.querySelector(".totalh1");
+total.innerHTML = `Total : Rs.${totalAmount}`;
+    
   }
 }
 
@@ -33,15 +41,24 @@ let addExpense = document.querySelector("#Add");
 addExpense.addEventListener("click", function(){
  let inputAmount = document.querySelector(".input--amount");
   let inputDesc = document.querySelector(".input--desc");
+  let inputQuantity = document.querySelector(".input--quantity")
 
   let inputAmountTrim = parseInt(inputAmount.value.trim());
   let inputDescTrim = inputDesc.value.trim();
-
-  if (inputAmountTrim > 0 && inputDescTrim !== "") {
-    expenseList.push({ amount: inputAmountTrim, desc: inputDescTrim });
+  let inputQuantityTrim = parseInt(inputQuantity.value.trim());
+ 
+  let quantityIfZero = inputQuantityTrim || 1;
+  if (inputAmountTrim > 0 && inputDescTrim !== "" && inputAmountTrim >= 1) {
+     
+    expenseList.push({ 
+      amount: inputAmountTrim,
+      quantity: quantityIfZero,
+      desc: inputDescTrim 
+    });
     localStorage.setItem("expense", JSON.stringify(expenseList));
     inputAmount.value = "";
     inputDesc.value = "";
+    inputQuantity.value = "";
     render();
   }
 }); // ✅ Function reference only
